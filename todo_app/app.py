795808b17e55772d5f8a8f5f6c41d63474cfa3f9
Asad Omar <asad.omar@yahoo.co.uk>
2021-12-03 17:18:31 +0000

    Added functionality to create new todo items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from todo_app.data.session_items import get_items
+from flask import Flask, render_template, request, redirect, url_for
+from todo_app.data.session_items import get_items, add_item
 
 from todo_app.flask_config import Config
 
@@ -11,3 +11,13 @@ app.config.from_object(Config())
 def index():
     items = get_items()
     return render_template("index.html", items=items)
+
+@app.route('/item', methods = ['POST', 'GET'])
+def add_task():
+    if request.method == "POST":
+        new_item = request.form.get("title")
+        add_item(new_item)
+        return redirect(url_for('index'))
+    else:
+        return render_template("index.html")
+
