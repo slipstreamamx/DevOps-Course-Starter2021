3dde0d29b7adcb6409f3e6632911c48de2d3d462
Asad Omar <asad.omar@yahoo.co.uk>
2021-12-05 13:34:11 +0000

    Added functionality to mark an item as completed

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from todo_app.data.session_items import get_items, add_item
+from todo_app.data.session_items import get_items, add_item, get_item, save_item
 
 from todo_app.flask_config import Config
 
@@ -17,6 +17,17 @@ def add_task():
     if request.method == "POST":
         new_item = request.form.get("title")
         add_item(new_item)
+        return redirect(url_for('index'))
+    else:
+        return render_template("index.html")
+
+@app.route('/update', methods = ['POST', 'GET'])
+def complete_task():
+    if request.method == "POST":
+        completed_item = request.form.get("mark complete")
+        update_item = get_item(completed_item)
+        update_item["status"] = "Completed"
+        save_item(update_item)
         return redirect(url_for('index'))
     else:
         return render_template("index.html")
