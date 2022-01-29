a5043b52ba56234d5ab227f55ec7c7a08dd89d6a
Asad Omar <asad.omar@yahoo.co.uk>
2022-01-29 17:21:06 +0000

    Added Class Item

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -1,11 +1,8 @@
+from todo_app.data.todo_items import Item
 from urllib import response
 import requests, os
 from dotenv import load_dotenv
 load_dotenv()
-
-
-board_id = os.getenv("TRELLO_BOARD_ID")
-
 
 def get_auth_params():
     """
@@ -23,16 +20,33 @@ def build_url(endpoint):
     """
     return os.getenv("TRELLO_BASE_URL") + endpoint
 
+def build_params(params={}):
+    full_params = get_auth_params()
+    full_params.update(params)
+    return full_params
 
-def get_list_of_boards():
-    params = get_auth_params()
-    url = build_url('/members/me/boards?fields=name,url')
-    response = requests.get(url, params=params)
-    boards_info =  response.json()
-    return boards_info
+def get_lists():
+    params = build_params({'cards': 'open'})
+    url = build_url('/boards/%s/lists' % os.getenv("TRELLO_BOARD_ID"))
+    response = requests.get(url, params = params)
+    lists = response.json()
+    return lists
+
+def get_items():
+    lists = get_lists()
+    items = []
+    for card_list in lists:
+        for card in card_list['cards']:
+            items.append(Item.fromTrelloCard(card, card_list))
+    print(items)
+    return items
+get_items()
 
 
-get_list_of_boards()
+
+
+
+
 
 
 
