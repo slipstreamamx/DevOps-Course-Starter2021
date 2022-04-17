518912ba5cc0db33689b44cdf724d07a3ffa04ff
Asad Omar <34609286+slipstreamamx@users.noreply.github.com>
2022-04-17 03:02:41 +0100

    Refectored App and Index

--- todo_app/app.py
+++ todo_app/app.py
@@ -9,62 +9,27 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    """
-    Returns the list of saved todo items.
-    """
     items = get_items()
     item_view_model = ViewModel(items)
-    return render_template("index.html", view_model=item_view_model)
+    return render_template('index.html', view_model=item_view_model)
 
-@app.route('/item', methods = ['POST'])
-def add_new_item():
-    """
-    Returns the list of saved todo items from Trello web app. Redirects the user back to the index page
+@app.route('/items/new', methods=['POST'])
+def add_item():
+    name = request.form['name']
+    add_item(name)
+    return redirect(url_for('index'))
 
-    """
-    if request.method == "POST":
-        title = request.form.get("title")
-        desc = request.form.get("desc text")
-        due = request.form.get("date")
-        add_item(title, desc, due)
-        return redirect(url_for('index'))
+@app.route('/items/<item_id>/in_progress')
+def set_item_to_progress(item_id):
+    item_in_progress(item_id)
+    return redirect(url_for('index'))
 
-@app.route('/complete', methods = ['POST', 'GET'])
-def set_item_to_complete():
-    """
-    Marks an item as completed. Redirects the user back to the index page
+@app.route('/items/<item_id>/complete')
+def set_item_to_complete(item_id):
+    item_completed(item_id)
+    return redirect(url_for('todo.index'))
 
-    """
-    if request.method == "POST":
-        completed_item = request.form.get("mark as completed")
-        item_completed(completed_item)
-        return redirect(url_for('index'))
-    else:
-        return render_template("index.html")
-
-@app.route('/inprogress', methods = ['POST', 'GET'])
-def set_item_to_progress():
-    """
-    Sets an item as in progresss. Redirects the user back to the index page
-
-    """
-    if request.method == "POST":
-        in_progress = request.form.get("set to progress")
-        item_in_progress(in_progress)
-        return redirect(url_for('index'))
-    else:
-        return render_template("index.html")
-
-@app.route('/reset', methods = ['POST', 'GET'])
-def set_item_status():
-    """
-    Resets an item status.
-
-    """
-    if request.method == "POST":
-        reset_item = request.form.get("Reset item status")
-        reset_item_status(reset_item)
-        return redirect(url_for('index'))
-    else:
-        return render_template("index.html")
-
+@app.route('/items/<item_id>/reset') 
+def set_item_status(item_id):    
+    reset_item_status(item_id)
+    return redirect(url_for('todo.index'))
