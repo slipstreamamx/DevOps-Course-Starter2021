32efb9827c8188ecb123793557c8f92e54afe906
Asad Omar <34609286+slipstreamamx@users.noreply.github.com>
2022-04-30 17:49:46 +0100

    fixed a bug wit reset item

--- todo_app/app.py
+++ todo_app/app.py
@@ -4,34 +4,41 @@ from todo_app.data.trello_items import get_items, add_item, item_in_progress, it
 from todo_app.flask_config import Config
 from todo_app.view_model import ViewModel
 
-app = Flask(__name__)
-app.config.from_object(Config())
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config())
+  
 
-@app.route('/')
-def index():
-    items = get_items()
-    item_view_model = ViewModel(items)
-    return render_template('index.html', view_model=item_view_model)
+    @app.route('/')
+    def index():
+        items = get_items()
+        item_view_model = ViewModel(items)
+        return render_template('index.html', view_model=item_view_model)
 
-@app.route('/items/new', methods=['POST'])
-def add_new_item():
-    name = request.form['name']
-    desc = request.form.get("desc text")
-    due = request.form.get("date")
-    add_item(name, desc, due)
-    return redirect(url_for('index'))
+    @app.route('/items/new', methods=['POST'])
+    def add_new_item():
+        name = request.form['name']
+        desc = request.form.get("desc text")
+        due = request.form.get("date")
+        add_item(name, desc, due)
+        return redirect(url_for('index'))
 
-@app.route('/items/<item_id>/in_progress')
-def set_item_to_progress(item_id):
-    item_in_progress(item_id)
-    return redirect(url_for('index'))
+    @app.route('/items/<item_id>/in_progress')
+    def set_item_to_progress(item_id):
+        item_in_progress(item_id)
+        return redirect(url_for('index'))
 
-@app.route('/items/<item_id>/complete')
-def set_item_to_complete(item_id):
-    item_completed(item_id)
-    return redirect(url_for('index'))
+    @app.route('/items/<item_id>/complete')
+    def set_item_to_complete(item_id):
+        item_completed(item_id)
+        return redirect(url_for('index'))
 
-@app.route('/items/<item_id>/reset') 
-def set_item_status(item_id):    
-    reset_item_status(item_id)
-    return redirect(url_for('todo.index'))
+    @app.route('/items/<item_id>/reset') 
+    def set_item_status(item_id):    
+        reset_item_status(item_id)
+        return redirect(url_for('index'))
+    
+    return app
+
+if __name__ == '__main__':
+    create_app().run()
