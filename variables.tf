be9fe0092a2e316f4b40d4f6e3576c6959dda6b3
Asad Omar <34609286+slipstreamamx@users.noreply.github.com>
2023-01-06 11:21:57 +0000

    Terraform deployment changes

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "client_id" {
+    type = string
+    description = "The oAuth client ID value for connection"
+    sensitive = true
+}
+
+variable "client_secret" {
+    type = string
+    description = "The oAuth client secret value for connection"
+    sensitive = true
+}
+
+variable "secret_key" {
+    type = string
+    description = "The secret key value"
+    sensitive = true
+}
+
+variable "azure_client_secret" {
+  description = "The client secret value for azure connection"
+  sensitive = true
+}
+
+variable "azure_client_id" {
+  description = "The client ID for the azure provider"
+}
+
+variable "azure_subscription_id" {
+  description = "The subscription ID for the azure provider"
+}
+
+variable "azure_tenant_id" {
+  description = "The tenant ID for the azure provider"
+
+}
